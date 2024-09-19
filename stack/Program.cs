1cdd3f46fe3d70cc0567e4a583964ccb4a01aeac
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-19 09:06:23 +0400

    Reverse a String Using a Stack

--- stack/Program.cs
+++ stack/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace stack
+﻿using System.Collections;
+using System.Linq;
+namespace stack
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("- - - Reverse a String Using a Stack - - - ");
+            Stack<string>reverse = new Stack<string>();
+            Console.WriteLine("Enter word");
+            string word=Console.ReadLine();
+            List<string> words = new List<string>();
+            foreach (char c in word)
+            {
+
+               reverse.Push(c.ToString());
+            }
+
+            foreach (string s in reverse)
+            {
+                words.Add(s);
+                Console.Write(s);
+            }
+
         }
+       
     }
 }
